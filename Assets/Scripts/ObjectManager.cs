2c53c417d2b3110357280d66cce050cdc2f33bd6
soapinduty <g4mrator@googlemail.com>
2020-03-05 10:37:04 +0100

    ObjectManager added.

---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    [Header("Attributes")]
+    public bool Spawning = false;
+    [Header("References")]
+    public List<GameObject> PrefabObjects = new List<GameObject>();
+
+    public void SpawnObject(GameObject prefab)
+    {
+
+    }
+
+    public void DespawnObject(GameObject obj)
+    {
+
+    }
+}
