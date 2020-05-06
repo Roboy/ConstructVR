407077247f1f9993e5519e3c21b7aa2abf45fa68
soapinduty <g4mrator@gmail.com>
2020-05-06 09:49:10 +0200

    TUM added.

---
+++ Assets/Scripts/TrainingUnitManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Construct.Utilities
+{
+    //Is unique, one per training unit, depends on what content should be taught
+    //so e.g. vestUnitManager inherits from TrainingUnitManager
+    public class TrainingUnitManager : Singleton<TrainingUnitManager>
+    {
+        public bool unitGoalComplete = false;
+        //Cues, hints the operator is getting
+        public List<string> instructions;
+    }
+}
